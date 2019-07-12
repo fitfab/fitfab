f03e92440b93e5015f187bcc637f62b416a9ad77
Miguel Julio <fitfab@yahoo.com>
2019-07-12 16:20:14 -0400

    add some sample code from react-style

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,24 +1,49 @@
 import React from "react";
 import { Button } from "@fitfab/ui";
-
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import styled from "styled-components";
 import "./App.css";
 
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+const SampleCode = styled.div`
+  > div:first-child {
+    background: #17223b;
 
-const Sample = () => {
-  return <strong>Hello World!</strong>;
-};
+    margin: 20px;
+    padding: 20px;
+  }
+
+  > div:last-child {
+    border: 1px solid #6b778d;
+    margin: 20px;
+    padding: 20px;
+  }
+`;
+
+const headerProps = { text: "I'm styled!" };
+const scope = { styled, headerProps };
+const code = `
+  // this a button
+  const Btn = styled.button\`
+    color: palevioletred;
+    font-size: 18px;
+  \`
+
+  render(<Btn>{headerProps.text}</Btn>)
+`;
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <header className="content">
         <h1>fitfab by miguel julio</h1>
-        <LiveProvider code="<p>hello</p>">
-          <LiveEditor />
-          <LiveError />
-          <LivePreview />
-        </LiveProvider>
+        <SampleCode>
+          <LiveProvider code={code} scope={scope} noInline={true}>
+            <LiveEditor />
+            <LiveError />
+            <LivePreview />
+          </LiveProvider>
+        </SampleCode>
+
         <Button>hello button</Button>
       </header>
     </div>
