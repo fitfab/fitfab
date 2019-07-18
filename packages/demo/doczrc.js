51274f846e2020971d3aa7deb2bf4028b875c6fd
Miguel Julio <fitfab@yahoo.com>
2019-07-18 13:34:32 -0400

    add more config

---
+++ packages/demo/doczrc.js
@@ -0,0 +1,17 @@
+export default {
+  title: "fitfab UI",
+  description: "UI components by miguel julio",
+  base: "/",
+  typescript: true,
+  propsParser: true,
+  codeSandbox: false,
+  themeConfig: {
+    showPlaygroundEditor: true,
+    colors: {
+      primary:
+        "linear-gradient(to right, rgb(146, 254, 157) 0%, rgb(0, 201, 255) 100%);"
+    }
+  },
+
+  menu: ["Home"]
+};
--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
+export interface Props {
+  width: string;
+}
+
+export const Button = styled.button<Props>`
   background: #f90587;
   color: #fff;
   border: none;
@@ -11,6 +15,7 @@ export const Button = styled.button`
   padding: 8px 24px;
   text-transform: uppercase;
   transition: all 0.5s;
+  width: ${({ width }) => (width ? width : "100px")};
 
   &:hover {
     background: #f33588;
